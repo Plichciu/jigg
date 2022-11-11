0d0e21e5c270a844517aed16a293cb664d51dfa2
Michał <lmichal02@o2.pl>
2022-11-11 01:17:07 +0100

    fix download name

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function ExportToExcel(type, fn, dl) {
 	const wb = XLSX.utils.table_to_book(elt, { sheet: 'sheet1' })
 	return dl
 		? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-		: XLSX.writeFile(wb, fn || 'MySheetName.' + (type || 'xlsx'))
+		: XLSX.writeFile(wb, fn || 'profiles.' + (type || 'xlsx'))
 }
 
 const generateBtn = document.getElementById('generateBtn')
